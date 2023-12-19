510fe8009999ea971cbc27ddb5dfc87edaddf97e
Enoch Kibet <enochblake9@gmail.com>
2023-12-19 18:28:06 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(destination) {
+    const hq = 42;
+    return Math.abs(destination - hq);
+  }
+  
+  function distanceFromHqInFeet(destination) {
+    const blocks = distanceFromHqInBlocks(destination);
+    return blocks * 264; // 1 block = 264 feet
+  }
+  
+  function distanceTravelledInFeet(start, destination) {
+    const distance = Math.abs(destination - start) * 264;
+    return distance;
+  }
+  
+  function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+  
+    if (distance <= 400) {
+      return 0;
+    } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
+  
+  module.exports = {
+    distanceFromHqInBlocks,
+    distanceFromHqInFeet,
+    distanceTravelledInFeet,
+    calculatesFarePrice,
+  };
+  
